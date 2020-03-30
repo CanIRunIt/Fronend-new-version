b988f19424eab2f1ca8a0bd070d86df28247f4b1
C4Jay <choolakejay@gmail.com>
2020-03-30 21:52:16 +0530

    checkout-sum

--- src/components/Rigbuilder/rigbuilder.js
+++ src/components/Rigbuilder/rigbuilder.js
@@ -2,18 +2,21 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
 import PricesCard from './card';
-import { Button, IconButton } from '@material-ui/core';
+import { Button, IconButton, Card, CardContent } from '@material-ui/core';
 import { Plus } from 'react-bootstrap-icons';
 
 var cpupricelist = []
 var gpupricelist = []
+var checkouts = []
 
 class Rigbuilder extends Component {
 
     state = {
         cpu: '',
         gpu: '',
-        ram: ''
+        ram: '',
+        show: false,
+
     }
 
     componentDidMount () {
@@ -40,7 +43,33 @@ class Rigbuilder extends Component {
 
     }
 
+    plustoHandler  (comp, price)  {
+        var unitprice = {
+            rigcomp : comp,
+            compprice : price 
+        }
+
+        checkouts.push(unitprice)
+
+    }
+
+    showcheckout () {
+        this.setState({
+            show: true
+        })
+    }
+
     render () {
+
+        let checkoutsummary = null
+
+        if(this.state.show){
+            checkoutsummary = <div style={{marginTop: '8px'}}>{checkouts.map(singlechekcout => {
+                return <div className="container"><Card><CardContent><h5>{singlechekcout.rigcomp} : {singlechekcout.compprice}</h5></CardContent></Card></div>
+                    
+            })}</div>
+                
+        }
 
 
         return (
@@ -65,9 +94,10 @@ class Rigbuilder extends Component {
 
          {gpupricelist.map(gpu => {
                 if(gpu.GPU.replace(' @', '').substring(0, 16) == this.state.gpu.substring(0, 16)){
+                    let Price = gpu.Price
                     return <div style={{marginTop: '10px'}}><h1>{this.state.gpu}: Price comparison</h1>
                     <div>
-                        <div><PricesCard cpu={this.state.gpu} company="newegg" price={gpu.Price}></PricesCard><span><IconButton aria-label="delete" size="large">
+                        <div><PricesCard cpu={this.state.gpu} company="newegg" price={gpu.Price}></PricesCard><span><IconButton onClick={() => this.plustoHandler(this.state.gpu, Price)} aria-label="delete" size="large">
           <Plus fontSize="inherit" />CART
         </IconButton></span> </div>
                         <div><PricesCard cpu={this.state.gpu} company="ali" price={gpu.Price}></PricesCard><span><IconButton aria-label="delete" size="large">
@@ -77,6 +107,8 @@ class Rigbuilder extends Component {
                     </div>
                 }
             })}
+            <Button style={{backgroundColor: '#bd00ff', fontFamily: 'Tomorrow'}} onClick={() => this.showcheckout()}>CHECK THE SUM</Button>
+            {checkoutsummary}
  
             {/* <h1>{this.state.cpu}</h1> */}
             
